cd8dc3b19f65993b21a5b26aad2d00e5914bc713
ahmed-abmouty <ahmed122727727@gmail.com>
2023-09-28 10:22:55 +0300

    uploadin my project

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+void main() {
+//                            Primitive data Type
+/*                 
+          int     : عدد صحيح 1 2 3 3
+          double   : عدد صحيح وعشري 
+          String   : نصوص
+ */
+//                   Non Primitive data Type (complex data type)
+/**
+ * bool         : true ,false
+ * dynamic      : صحيح عشري نصصوص true false
+ */
+//                               Key word data type
+/**
+ * var    : true false , text, int , double 
+ */
+
+  int x = 5;
+  String name;
+  name = "mohamed";
+  double y = 5;
+  bool z = false;
+  var o = true;
+  int i;
+  i = 10;
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+  print(o);
+}
